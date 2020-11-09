a8df30012addde8754d4e69ef476fde89c35c4c8
Nico <nmanograsso@gmail.com>
2020-11-08 22:27:56 -0300

    Configure react and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: './src/index.jsx',
   output: {
     filename: 'app.bundle.js',
   },
   plugins: [
-    new HtmlWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
