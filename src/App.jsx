b1afa95a3832db7a7f56cba82aaa52df0e5974b4
Nico <nmanograsso@gmail.com>
2020-11-14 00:29:12 -0300

    add logo to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ListOfCategories from './components/ListOfCategories';
 import ListOfPhotoCards from './components/ListOfPhotoCards';
+import Logo from './components/Logo';
 
 const App = () => (
   <>
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </>
