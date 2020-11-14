47fa0e6e6e4fa82751fa7ccbc64dbeba068bd31c
Nico <nmanograsso@gmail.com>
2020-11-13 22:14:35 -0300

    get truthy data to categories

--- src/components/ListOfCategories/index.jsx
+++ src/components/ListOfCategories/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Category from '../Category';
 import './listOfCategories.scss';
+import { categories } from '../../../api/db.json';
 
 const ListOfCategories = () => (
   <ul className="listOfCategories">
-    {[1, 2, 3, 4, 5, 6, 7, 8].map((category) => (
+    {categories.map((category) => (
       <li className="listOfCategories__item">
-        <Category />
+        <Category cover={category.cover} emoji={category.emoji} path={category.path} />
       </li>
     ))}
   </ul>
