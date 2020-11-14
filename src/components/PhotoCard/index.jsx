833b555eb18756e7c77a02466ea6a997fa3a0fda
Nico <nmanograsso@gmail.com>
2020-11-14 00:52:16 -0300

    Make photo card bigger

--- src/components/PhotoCard/index.jsx
+++ src/components/PhotoCard/index.jsx
@@ -6,7 +6,7 @@ import { AiOutlineHeart } from 'react-icons/ai';
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
 const PhotoCard = ({ src, likes }) => (
-  <article>
+  <article className="photoCard">
     <div className="photoCard__image-container">
       <img src={src} alt="photoCard" className="photoCard__image" />
     </div>
