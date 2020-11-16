d9cb6c56d2e2707037845726fa345a2177fb116e
Nico <nmanograsso@gmail.com>
2020-11-15 23:38:52 -0300

    add loading style to categories

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import './category.scss';
+import PropTypes from 'prop-types';
 
 const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg';
 
-const Category = ({ cover = DEFAULT_IMAGE, emoji = '?', path = '' }) => (
-  <a className="category" href={path}>
-    <img className="category__image" src={cover} alt={emoji} />
-    {emoji}
-  </a>
-);
+const Category = (props) => {
+  const {
+    cover,
+    emoji,
+    path,
+    loading,
+  } = props;
+
+  if (loading) {
+    return (
+      <div className="category">
+        <div className="category__image loading" />
+        {emoji}
+      </div>
+    );
+  }
+
+  return (
+    <a className="category" href={path}>
+      <img className="category__image" src={cover} alt={emoji} />
+      {emoji}
+    </a>
+  );
+};
+
+Category.propTypes = {
+  cover: PropTypes.string,
+  emoji: PropTypes.string,
+  path: PropTypes.string,
+  loading: PropTypes.bool,
+};
+
+Category.defaultProps = {
+  cover: DEFAULT_IMAGE,
+  emoji: '😃',
+  path: '',
+  loading: false,
+};
 
 export default Category;
