e987997376435f8ffd09ba25d5bdf1d325e7e56d
Nico <nmanograsso@gmail.com>
2020-11-19 21:14:55 -0300

    Add query to list of photos and add loading state

--- src/components/ListOfPhotoCards/index.jsx
+++ src/components/ListOfPhotoCards/index.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 import PhotoCard from '../PhotoCard';
-import { photos } from '../../../api/db.json';
+import Loading from '../Loading';
 
-const ListOfPhotoCards = () => (
-  <ul>
-    {photos.map((photoCard) => (
-      <PhotoCard
-        key={photoCard.id}
-        src={photoCard.src}
-        likes={photoCard.likes}
-        id={photoCard.id}
-      />
-    ))}
-  </ul>
-);
+const getPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+}
+`;
+
+const ListOfPhotoCards = () => {
+  const { data, loading, error } = useQuery(getPhotos);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <p>Error</p>;
+  }
+
+  return (
+    <ul>
+      {data?.photos.map((photoCard) => (
+        <PhotoCard
+          key={photoCard.id}
+          src={photoCard.src}
+          likes={photoCard.likes}
+          id={photoCard.id}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default ListOfPhotoCards;
