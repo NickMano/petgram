07af6a6f860ab51f2ee46bb02e74de821d40f59f
Nico <nmanograsso@gmail.com>
2020-11-19 21:10:32 -0300

    add loading coponent

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Loading.scss';
+
+const Loading = () => (
+  <div className="lds-default__container">
+    <div className="lds-default">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  </div>
+);
+
+export default Loading;
