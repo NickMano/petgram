35b55396cb9e033e636c713a50af2e50fb7972b3
Nico <nmanograsso@gmail.com>
2020-11-26 08:17:32 -0300

    add style to fav-button!

--- src/components/FavButton/index.jsx
+++ src/components/FavButton/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import './favButton.scss';
 
 const FavButton = ({ liked, likes, onClick }) => {
   const IconHeart = liked ? AiFillHeart : AiOutlineHeart;
 
   return (
-    <button type="button" className="photoCard__button fade-in" onClick={onClick}>
-      <IconHeart size="24px" />
+    <button type="button" className="fav-button fade-in" onClick={onClick}>
+      <IconHeart className="fav-button--icon" size="28px" />
       {likes}
       {' '}
       likes!
