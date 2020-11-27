189980e4964b2096546834a08ccd62cfad33170e
Nico <nmanograsso@gmail.com>
2020-11-27 16:32:51 -0300

    add tab bar to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Router } from '@reach/router';
 import Home from './pages/Home';
 import PhotoDetail from './pages/PhotoDetail';
+import TabBar from './components/TabBar';
 
 const App = () => (
-  <Router>
-    <Home path="/" />
-    <PhotoDetail path="/photo/:photoId" />
-  </Router>
+  <>
+    <Router>
+      <Home path="/" />
+      <PhotoDetail path="/photo/:photoId" />
+    </Router>
+    <TabBar />
+  </>
 );
 
 export default App;
---
+++ src/components/TabBar/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './style.scss';
+import { BsPerson } from 'react-icons/bs';
+import { AiOutlineHeart, AiFillHome } from 'react-icons/ai';
+import { Link } from '@reach/router';
+
+const TabBar = () => {
+  const SIZE = '28px';
+
+  return (
+    <div className="tab-bar">
+      <Link to="/" className="tab-bar__button">
+        <AiFillHome size={SIZE} />
+      </Link>
+      <Link to="/favs" className="tab-bar__button">
+        <AiOutlineHeart size={SIZE} />
+      </Link>
+      <Link to="/user" className="tab-bar__button">
+        <BsPerson size={SIZE} />
+      </Link>
+    </div>
+  );
+};
+
+export default TabBar;
