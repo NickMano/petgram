03dfc7881bb0b815617c45f64f3bff9fe2383ee1
Nico <nmanograsso@gmail.com>
2020-11-30 18:09:15 -0300

    add validation to form

--- src/components/UserForm/index.jsx
+++ src/components/UserForm/index.jsx
@@ -2,16 +2,42 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import useInputForm from '../../hooks/useInputForm';
 
+const isValidMail = (mail) => new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g).test(mail);
+
 const UserForm = ({ onSubmit }) => {
-  const password = useInputForm();
-  const email = useInputForm();
+  const password = useInputForm('');
+  const mail = useInputForm('');
+  const cssClassName = 'user-form';
+  const [mailClassName, setMailClassName] = useState(`${cssClassName}--input`);
+  const handleSubmit = (e) => {
+    setMailClassName(`${cssClassName}--input`);
+    let isValidUser = true;
+
+    if (mail.value.trim().length === 0 || !isValidMail(mail.value)) {
+      isValidUser = false;
+      setMailClassName(`${cssClassName}--input error`);
+    }
+
+    e.preventDefault();
+
+    if (!isValidUser) {
+      console.log('error');
+    } else {
+      setTimeout(() => {
+        onSubmit();
+      }, 1000);
+    }
+  };
 
   return (
-    <form onSubmit={onSubmit}>
+    <div className={cssClassName}>
       <input placeholder="Email" value={email.value} onChange={email.onChange} />
-      <input placeholder="Password" ype="password" value={password.value} onChange={password.onChange} />
-      <button type="submit">Login</button>
-    </form>
+      <form onSubmit={handleSubmit} className={`${cssClassName}--form`}>
+        <input placeholder="Mail" value={mail.value} onChange={mail.onChange} className={mailClassName} />
+        <input placeholder="Password" type="password" value={password.value} onChange={password.onChange} className="user-form--input" />
+        <button type="submit" className="user-form--button">Login</button>
+      </form>
+    </div>
   );
 };
 
