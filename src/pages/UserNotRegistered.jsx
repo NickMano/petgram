a3a4eb610756c097e4239dd1b29b99948da33010
Nico <nmanograsso@gmail.com>
2020-12-02 12:57:15 -0300

    hardy brehabiour of handle subit with mutation

--- src/pages/UserNotRegistered.jsx
+++ src/pages/UserNotRegistered.jsx
@@ -1,19 +1,36 @@
 import React, { useContext } from 'react';
+import { useMutation } from '@apollo/react-hooks';
 import UserForm from '../components/UserForm';
 import LoginContext from '../context/LoginContext';
 import NavBar from '../components/NavBar';
+import { signUpMutation } from '../queries';
 
 const UserNotRegistered = () => {
   const { isAuth, setIsAuth } = useContext(LoginContext);
+  const [signUp] = useMutation(signUpMutation);
 
-  const handleSubmit = () => {
-    setIsAuth(!isAuth);
+  const handleSubmit = async (email, password) => {
+    const userCredentials = {
+      variables: {
+        input: {
+          email,
+          password,
+        },
+      },
+    };
+
+    try {
+      await signUp(userCredentials);
+      setIsAuth(!isAuth);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div style={{ paddingBottom: '50px' }}>
       <NavBar />
-      <UserForm onSubmit={handleSubmit} />
+      <UserForm onSubmit={handleSubmit} buttonTitle="Sign Up" />
     </div>
   );
 };
