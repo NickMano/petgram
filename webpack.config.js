a504e23783db7e760b00fdf1a1cb4a25b6c4e696
Nico <nmanograsso@gmail.com>
2020-12-05 10:32:23 -0300

    add config to do PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackPwaPlugin = require('webpack-pwa-manifest');
+const path = require('path');
 
 module.exports = {
   entry: './src/index.jsx',
@@ -16,6 +18,19 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css',
+    }),
+    new WebpackPwaPlugin({
+      name: 'Petgram - Your App of pet\'s photos',
+      short_name: 'Petgram',
+      description: 'With Petgram you can find photos of pets easily',
+      background_color: '#fff',
+      theme_color: '#07BEB8',
+      icons: [{
+        src: path.resolve('src/assets/icon.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+        type: 'image/png',
+        purpose: 'any maskable',
+      }],
     }),
   ],
   module: {
